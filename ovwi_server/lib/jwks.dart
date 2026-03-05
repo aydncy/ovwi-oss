26931fe580aea5f1194b8809ce5b6de55ae337d1
aydncy <aydinceylan07@gmail.com>
2026-03-05 03:58:50 +0000

    restore jwks builder

--- ovwi_server/lib/jwks.dart
+++ ovwi_server/lib/jwks.dart
@@ -1,22 +1,19 @@
 import 'dart:convert';
-import 'package:shelf/shelf.dart';
 
-Response jwksHandler(Request request) {
-  final jwks = {
+Map<String, dynamic> buildJwks(List<int> publicKey, String kid) {
+
+  final x = base64UrlEncode(publicKey).replaceAll("=", "");
+
+  return {
     "keys": [
       {
         "kty": "OKP",
         "crv": "Ed25519",
         "use": "sig",
         "alg": "EdDSA",
-        "kid": "ovwi-key-1",
-        "x": "vd6T3Ck3bV9GvFvX7n8P3gGxqXW6nB9H7d6R5zK1q2U"
+        "kid": kid,
+        "x": x
       }
     ]
   };
-
-  return Response.ok(
-    jsonEncode(jwks),
-    headers: {"content-type": "application/json"},
-  );
 }
